46eab61710964e5dcf7dabad68b9df8d5f3c92c0
Mega <v.v.humeniuk@student.khai.edu>
2025-03-27 00:53:27 +0200

    21

---
+++ Messaging/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Messaging
+{
+    public class Publisher
+    {
+        private readonly MessageQueue queue;
+
+        public Publisher(MessageQueue queue)
+        {
+            this.queue = queue;
+        }
+
+        public void SendMessage(string content)
+        {
+            var message = new Message(content);
+            queue.AddMessage(message);
+            Console.WriteLine($"Sent: {content}");
+        }
+    }
+}
---
+++ Messaging/Subscriber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Messaging
+{
+    public class Subscriber
+    {
+        private readonly MessageQueue queue;
+
+        public Subscriber(MessageQueue queue)
+        {
+            this.queue = queue;
+        }
+
+        public void ReceiveMessage()
+        {
+            var message = queue.GetMessage();
+            Console.WriteLine($"Received: {message.Content}");
+        }
+    }
+}
